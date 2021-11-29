15546067a91d8d5b3d1d5bb2f4112105d65f0740
Marcos Quixadá <marcos.quixada@yahoo.com.br>
2021-11-29 00:39:36 -0300

    Mocking first Unsplash API calls

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.scss";
 import "./animations.scss";
 import Img from "./photo.jpg";
+import { useState, useEffect } from "react";
 
 function App() {
-  
+  let [users, setUsers] = useState([]);
+
   const showModalFunc = () => {
     let array = ["one", "two", "five"];
     var buttonId = array[Math.floor(Math.random() * 3)];
-    
+
     document.getElementById("modal-container").removeAttribute("class");
     document.getElementById("modal-container").classList.add(buttonId);
     document.body.classList.add("modal-active");
@@ -18,6 +20,25 @@ function App() {
     document.body.classList.remove("modal-active");
   };
 
+  const loadMore = () => {
+    fetch(
+      "https://api.unsplash.com/photos/?client_id=1iU80BRXpOFHRDk1itZWN3wDONPS21lSZqj6drllJZ0"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setUsers([...users, ...data])
+      });
+  };
+
+  useEffect(() => {
+    fetch(
+      "https://api.unsplash.com/photos/?client_id=1iU80BRXpOFHRDk1itZWN3wDONPS21lSZqj6drllJZ0"
+    )
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+
   return (
     <div className="container">
       <div className="row">
@@ -26,54 +47,32 @@ function App() {
           <input className="search" type="text" placeholder="Search..." />
         </div>
       </div>
-      <div className="row">
-        <div className="card" onClick={(e) => showModalFunc()}>
-          <img src={Img} alt="" />
-          <span className="user-name">MARK LANDBERG</span>
-          <span className="user-desc">
-            Lorem ipsum dolor sit amet, consectetur…
-          </span>
-        </div>
-        <div className="card" onClick={(e) => showModalFunc()}>
-          <img src={Img} alt="" />
-          <span className="user-name">MARK LANDBERG</span>
-          <span className="user-desc">
-            Lorem ipsum dolor sit amet, consectetur…
-          </span>
-        </div>
-        <div className="card" onClick={(e) => showModalFunc()}>
-          <img src={Img} alt="" />
-          <span className="user-name">MARK LANDBERG</span>
-          <span className="user-desc">
-            Lorem ipsum dolor sit amet, consectetur…
-          </span>
-        </div>
+      <div className="main-content">
+        {users.map((user) => (
+          <ul>
+            <li className="card">
+              <img
+                className="card-image"
+                src={
+                  user.user &&
+                  user.user.profile_image &&
+                  user.user.profile_image.medium
+                }
+                alt=""
+              />
+              <h2 className="card-title">{user.user && user.user.name}</h2>
+              <p className="card-subtitle">
+                Lorem ipsum dolor sit amet, consectetur…
+              </p>
+            </li>
+          </ul>
+        ))}
       </div>
-      <div className="row">
-        <div className="card" onClick={(e) => showModalFunc()}>
-          <img src={Img} alt="" />
-          <span className="user-name">MARK LANDBERG</span>
-          <span className="user-desc">
-            Lorem ipsum dolor sit amet, consectetur…
-          </span>
-        </div>
-        <div className="card" onClick={(e) => showModalFunc()}>
-          <img src={Img} alt="" />
-          <span className="user-name">MARK LANDBERG</span>
-          <span className="user-desc">
-            Lorem ipsum dolor sit amet, consectetur…
-          </span>
-        </div>
-        <div className="card" onClick={(e) => showModalFunc()}>
-          <img src={Img} alt="" />
-          <span className="user-name">MARK LANDBERG</span>
-          <span className="user-desc">
-            Lorem ipsum dolor sit amet, consectetur…
-          </span>
-        </div>
-      </div>
+
       <div className="row load-more">
-        <button className="button">LOAD MORE</button>
+        <button className="button" onClick={(e) => loadMore()}>
+          LOAD MORE
+        </button>
       </div>
 
       <div id="modal-container">
@@ -111,42 +110,6 @@ function App() {
           </div>
         </div>
       </div>
-
-      {/* {
-        <div id="myModal" class="modal">
-          <div class="modal-content">
-            <div className="row">
-              <span className="edit-user">Edit user</span>
-            </div>
-            <div className="row map-form">
-              <div className="map"></div>
-              <div className="form">
-                <label for="name" className="label-form">
-                  Name
-                </label>
-                <input id="name" className="input-form" type="text" />
-                <label for="address" className="label-form">
-                  Address
-                </label>
-                <input id="address" className="input-form" type="text" />
-                <label for="description" className="label-form">
-                  Description
-                </label>
-                <input id="description" className="input-form" type="text" />
-                <div className="buttons">
-                  <input type="button" value="Save" className="button" />
-                  <input
-                    type="button"
-                    className="button"
-                    value="Cancel"
-                    onClick={(e) => modal(false, "auto")}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      } */}
     </div>
   );
 }
